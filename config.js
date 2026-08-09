ee5815e285b04624c73feac510b866ea3436ec7d
wixgung <wixgung@gmail.com>
2026-08-09 20:40:36 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // KONFIGURASI PORTAL — edit HANYA baris di bawah ini
 // Tempel URL Web App Apps Script kamu (lihat PANDUAN-SPREADSHEET.md)
 // ============================================================
-const CONTROL_URL = "https://script.google.com/macros/s/GANTI_DENGAN_ID_DEPLOYMENT/exec";
+const CONTROL_URL = "https://script.google.com/macros/s/AKfycbwUvOW_-P0ikJ7GDjgdFf61CP-r4T-4OVW72pz0euivnbeDuZslY27mhXmO-IOd6bo/exec";
