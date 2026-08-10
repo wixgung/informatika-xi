d5fab05af07b71aacead7503a833713bc80932c6
wixgung <wixgung@gmail.com>
2026-08-10 20:38:08 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // KONFIGURASI PORTAL — edit HANYA baris di bawah ini
 // Tempel URL Web App Apps Script kamu (lihat PANDUAN-SPREADSHEET.md)
 // ============================================================
-const CONTROL_URL = "https://script.google.com/macros/s/AKfycbwUvOW_-P0ikJ7GDjgdFf61CP-r4T-4OVW72pz0euivnbeDuZslY27mhXmO-IOd6bo/exec";
+const CONTROL_URL = "https://script.google.com/macros/s/AKfycbyvVxCtdGqHyQHbDvTmEtTK513QpYyHGjOVvVgYLleEWS1YkMSUAlg5OvF1QpenCz6q/exec";
